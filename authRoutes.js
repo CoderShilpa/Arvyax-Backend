cf8826c5a445d48ab2ac7f1767fb95911185c206
Shilpa Kumari <152641069+CoderShilpa@users.noreply.github.com>
2025-08-01 14:04:18 +0530

    Update authRoutes.js

--- authRoutes.js
+++ authRoutes.js
@@ -1,12 +1,6 @@
-const cors = require("cors");
 const express = require("express");
 const router = express.Router();
 const { registerUser, loginUser } = require("../controllers/authController");
-
-const app = express();
-
-app.use(cors());
-app.use(express.json()); // ✅ This should come before your routes
 
 router.post("/register", registerUser);
 router.post("/login", loginUser);
